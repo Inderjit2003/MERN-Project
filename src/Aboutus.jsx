ca7487546de0699a35951f6cd3eb7345318716c1
Inderjit kaur <107792317+Inderjit2003@users.noreply.github.com>
2024-02-23 14:25:47 +0530

    modifyindex,createaboutus,app.jsx|23-02-2024|2:27

---
+++ src/Aboutus.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Aboutus() {
+  return (
+    <><h1>Welcome To MERN Stack</h1><h2>Hlo How are You</h2></>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+//App name of the file .is the same folder 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
